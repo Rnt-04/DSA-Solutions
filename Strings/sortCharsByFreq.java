0af0732ae1b0abb11210979f2f7ce61ae244a76c
Rnt-04 <ritintagore@gmail.com>
2025-07-06 21:05:43 +0530

    Update sortCharsByFreq.java

--- Strings/sortCharsByFreq.java
+++ Strings/sortCharsByFreq.java
@@ -7,6 +7,19 @@ public class sortCharsByFreq {
         String s=sc.nextLine();
         sc.close();
         //
-        
+        //using hashmap and list sorting
+        String ans="";
+        HashMap <Character, Integer> hm = new HashMap<>();
+        for (char ch: s.toCharArray()){
+            hm.put(ch,hm.getOrDefault(ch, 0)+1);
+        }
+        List <Character> list= new ArrayList(hm.keySet());
+        list.sort ((ob1,ob2) -> hm.get(ob2)-hm.get(ob1));
+        for (char ch:list){
+            for (int i=0;i<hm.get(ch);i++){
+                ans+=ch;
+            }
+        }
+        System.out.println (ans);
     }
 }
